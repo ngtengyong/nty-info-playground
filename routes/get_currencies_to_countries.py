5d5fcde6adf39331848ab94c69437df275bc9492
tengyong <tengyong@gmail.com>
2023-03-23 10:01:46 +0800

    update get currencies

--- routes/get_currencies_to_countries.py
+++ routes/get_currencies_to_countries.py
@@ -3,16 +3,17 @@ from google.cloud import datastore
 def get_currencies():
     client = datastore.Client()
     # get country codes
-    query = client.query(kind='country_to_watch')
-    entities = list(query.fetch())
-    country_codes = [entity['country_code'] for entity in entities]
+    query1 = client.query(kind='country_to_watch')
+    entities1 = list(query1.fetch())
+    country_codes = [entity1['country_code'] for entity1 in entities1]
     # get currency code of country codes above
-    query = client.query(kind='country_currency')
-    entities = list(query.fetch())
+    query2 = client.query(kind='country_currency')
+    entities2 = list(query2.fetch())
     currencies_to_countries = {}
-    for entity in entities:
-        if entity['country_code'] in country_codes:
-            currencies_to_countries[entity['currency_code']] = entity['country_name']
+    for entity2 in entities2:
+        #print (f'entity = {entity2["country_code"]}')
+        if entity2['country_code'] in country_codes:
+            currencies_to_countries[entity2['currency_code']] = entity2['country_name']
     # Print the results
     print('Currencies to W A T C H !:', currencies_to_countries.keys())
     return currencies_to_countries
